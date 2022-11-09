03d3af726b4750e12222a45befae12bd8319b179
Mara.X.Ma <maraxma@hotmail.com>
2022-11-09 15:23:34 +0800

    clean git cache

--- src/test/java/org/mosin/annohttp/httpservice/TestClientWithoutAnno.java
+++
@@ -1,9 +0,0 @@
-package org.mosin.annohttp.httpservice;
-
-import org.mosin.annohttp.annotation.Query;
-import org.mosin.annohttp.annotation.Request;
-
-public interface TestClientWithoutAnno {
-    @Request(url = "http://baidu.com")
-    String getItemName(@Query("ItemNo") String itemNo);
-}
